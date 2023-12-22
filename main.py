65a438de1ed1abf679c4269d114b4dbe5f7cdc3d
Korin <1878464932@qq.com>
2023-12-22 18:04:02 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from tinydb import TinyDB,Query
+
+class Filter(BaseModel):
+    type: str
+    province: str|None = None
+    city: str|None = None
+    district: str|None = None
+    insts: List[str]
+
+# class Infos(BaseModel):
+#     title: str
+#     description: List[str]
+#     type: str
+#     province: str|None = None
+#     city: str|None = None
+#     district: str|None = None
+#     inst: str
+
+db = TinyDB("data2.json")
+table = db.table("Infos")
+app = FastAPI()
+
+@app.post("/bandinfos")
+async def get_infos(filter:Filter):
+    Info = Query()
+    query_data = table.search(Info.type==filter.type and Info.province==filter.province and Info.city==filter.city and Info.district==filter.district and Info.insts.any(filter.insts))
+    search_result = query_data
+    return search_result
