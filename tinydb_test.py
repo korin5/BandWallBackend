0b8f6053528c55c5b1d068e39ed87447ccddcb05
Korin <1878464932@qq.com>
2023-12-22 18:04:21 +0800

    Create tinydb_test.py

---
+++ tinydb_test.py
@@ -0,0 +1,25 @@
+from tinydb import TinyDB
+
+
+db = TinyDB("data2.json")
+table = db.table("Infos")
+index = table.insert_multiple([
+    {
+        "title": "Atarayo",
+        "description": ["A J-Pop Band","招募鼓手"],
+        "type": "线下",
+        "province": "上海市",
+        "city": "上海市",
+        "district": "不限",
+        "insts": ["架子鼓"]
+    },
+    {
+        "title": "Mygo",
+        "description": ["A J-Punk Band","招募键盘手"],
+        "type": "线下",
+        "province": "上海市",
+        "city": "上海市",
+        "district": "不限",
+        "insts": ["键盘"]
+    }])
+print("添加索引：", index)
