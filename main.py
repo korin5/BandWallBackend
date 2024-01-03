908d34088feccb8cac977386b1cafe0776c5592b
Korin <1878464932@qq.com>
2024-01-03 15:28:40 +0800

    添加上传接口

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ class Filter(BaseModel):
     district: str|None = None
     insts: List[str]
 
-# class Infos(BaseModel):
-#     title: str
-#     description: List[str]
-#     type: str
-#     province: str|None = None
-#     city: str|None = None
-#     district: str|None = None
-#     inst: str
+class Info(BaseModel):
+    type: str
+    province: str|None = None
+    city: str|None = None
+    district: str|None = None
+    insts: List[str]
+    title: str
+    description: str
 
 db = TinyDB("data2.json")
 table = db.table("Infos")
@@ -30,3 +30,19 @@ async def get_infos(filter:Filter):
     query_data = table.search((Info.type==filter.type) & (Info.province==filter.province) & (Info.city==filter.city) & (Info.district==filter.district) & (Info.insts.any(filter.insts)))
     search_result = query_data
     return search_result
+
+@app.post("/upload")
+async def upload_infos(info:Info):
+
+    description_list = info.description.split("\n")
+
+    index = table.insert({
+        "title": info.title,
+        "description": description_list,
+        "type": info.type,
+        "province": info.province,
+        "city": info.city,
+        "district": info.district,
+        "insts": info.insts
+    })
+    print("添加索引：", index)
